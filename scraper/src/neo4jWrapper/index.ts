43a389aa39e9580180d8172772c8b544bb3c0e92
Jacky Zhao <j.zhao2k19@gmail.com>
2022-01-10 10:23:57 -0800

    base timestamp implementation

--- scraper/src/neo4jWrapper/index.ts
+++ scraper/src/neo4jWrapper/index.ts
@@ -42,7 +42,15 @@ async function createTx(tx: typeof neo4j.Transaction, data: TxI) {
     MERGE (b:Account {addr: $to})
     SET a.isUser = ${data.fromIsUser}
     SET b.isUser = ${data.toIsUser}
-    CREATE p = (a)-[:To { category: $category, blockNum: $blockNum, value: $value, asset: $asset, hash: $hash, distance: $distance}]->(b)
+    CREATE p = (a)-[:To {
+        category: $category,
+        blockNum: $blockNum,
+        value: $value,
+        asset: $asset,
+        hash: $hash,
+        distance: $distance,
+        timestamp: timestamp()
+    }]->(b)
     RETURN p
     `;
     return tx.run(template, data);
