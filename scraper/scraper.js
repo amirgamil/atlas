7eee7df21907640bf5828586fced194331f34df5
Matt Katz <mykatz01@gmail.com>
2022-01-09 17:59:10 -0800

    Scraping w/ pagination

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -37,7 +37,7 @@ async function get_erc20_transactions_for_block(blockNum) {
         params: [
           {
             fromBlock: "0x" + blockNum.toString(16),
-            toBlock: "0x" + (blockNum + 100).toString(16),
+            toBlock: "0x" + (blockNum + 20).toString(16),
             category: ["erc20"],
           },
         ],
@@ -47,8 +47,34 @@ async function get_erc20_transactions_for_block(blockNum) {
 
   const r = await res.json();
 
-  console.log(r);
-  console.log(r.result.transfers[10]);
+  let out = r.result.transfers;
+  let pageKey = r.result.pageKey;
+
+  while (pageKey != undefined) {
+    const result = await fetch(
+      "https://eth-mainnet.alchemyapi.io/v2/ZgihkMdrhmQNZJWJM2TLRNWez_AA5Jzo",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "alchemy_getAssetTransfers",
+          params: [
+            {
+              pageKey: r.result.pageKey,
+              fromBlock: "0x" + blockNum.toString(16),
+              toBlock: "0x" + (blockNum + 20).toString(16),
+              category: ["erc20"],
+            },
+          ],
+        }),
+      }
+    );
+    const j = await result.json();
+    out = out.concat(j.result.transfers);
+    pageKey = j.result.pageKey;
+  }
+
+  console.log(out);
 }
 
 async function main() {
