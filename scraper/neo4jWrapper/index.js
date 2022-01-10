0295fde99013cf3f14d2ad2977a49e03f77c2b2f
amirgamil <amir.amgad01@gmail.com>
2022-01-09 16:48:25 -0800

    fix .env

--- scraper/neo4jWrapper/index.js
+++ scraper/neo4jWrapper/index.js
@@ -1,21 +1,22 @@
 const { gql, ApolloServer } = require("apollo-server");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const neo4j = require("neo4j-driver");
-require("dotenv").config();
+require("dotenv").config({ path: "./scraper/neo4jWrapper/.env" });
 
 const typeDefs = gql`
-type Account {
-  addr: String!
-  tx: [Account!]! @relationship(type: "TO", properties: "Tx", direction: OUT)
-}
+    type Account {
+        addr: String!
+        tx: [Account!]!
+            @relationship(type: "TO", properties: "Tx", direction: OUT)
+    }
 
-interface Tx @relationshipProperties {
-  blockNum: String!
-  value: Float
-  asset: String
-  hash: String!
-  distance: Float!
-}
+    interface Tx @relationshipProperties {
+        blockNum: String!
+        value: Float
+        asset: String
+        hash: String!
+        distance: Float!
+    }
 `;
 
 // const insertTxAndAccount = gql`
@@ -69,16 +70,16 @@ interface Tx @relationshipProperties {
 // `
 
 const driver = neo4j.driver(
-  process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
 const server = new ApolloServer({
-  schema: neoSchema.schema,
+    schema: neoSchema.schema,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`GraphQL server ready on ${url}`);
+    console.log(`GraphQL server ready on ${url}`);
 });
