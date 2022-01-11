31a6111490208c4dcb33048499424f17a54902a4
Matt Katz <mykatz01@gmail.com>
2022-01-11 11:17:17 -0800

    lint

--- frontend/components/context.tsx
+++ frontend/components/context.tsx
@@ -1,42 +1,41 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Web3Modal from "web3modal";
 import { providers, Signer } from "ethers";
 
-
-
 export const AppContext = React.createContext({
-    openModal: () => {}
-})
+  openModal: () => {},
+});
 
 export const AppContextProvider = (props: any) => {
-    const [signer, setSigner] = useState<providers.JsonRpcSigner | undefined>(undefined);
-    const [address, setAddress] = useState<string | undefined>(undefined);
+  const [signer, setSigner] = useState<providers.JsonRpcSigner | undefined>(
+    undefined
+  );
+  const [address, setAddress] = useState<string | undefined>(undefined);
 
+  const openModal = async () => {
+    const providerOptions = {
+      /* See Provider Options Section */
+    };
 
-    const openModal = async () => {
-        const providerOptions = {
-        /* See Provider Options Section */
-        };
-        
-        const web3Modal = new Web3Modal({
-            network: "mainnet", // optional
-            cacheProvider: true, // optional
-            providerOptions // required
-        });
-        const provider = new providers.Web3Provider(await web3Modal.connect());
-        const signer = provider.getSigner();
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
+    const provider = new providers.Web3Provider(await web3Modal.connect());
+    const signer = provider.getSigner();
 
-        setSigner(signer);
+    setSigner(signer);
 
-        const address = await signer.getAddress();
-        setAddress(address);
-    }
+    const address = await signer.getAddress();
+    setAddress(address);
+  };
 
-    <AppContext.Provider
-      value={{
-        openModal
-      }}
-    >
-      <>{props.children}</>
-    </AppContext.Provider>
-}
+  <AppContext.Provider
+    value={{
+      openModal,
+    }}
+  >
+    <>{props.children}</>
+  </AppContext.Provider>;
+};
