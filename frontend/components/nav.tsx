1dfdc809192df750250a772856551032c5e5ec1c
Matt Katz <mykatz01@gmail.com>
2022-01-11 10:48:49 -0800

    Add tailwind, nav

---
+++ frontend/components/nav.tsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+        <div className="w-full h-20 flex">
+            <div className="text-2xl my-auto ml-16">Metis</div>
+            <div className="ml-auto my-auto mr-16">Sign In</div>
+        </div>
+    )
+}
+
+export default Nav;
