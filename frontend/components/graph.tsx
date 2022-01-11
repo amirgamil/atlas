e39e145a8a2b003e9e703dd796aadfff0d0552b8
Matt Katz <mykatz01@gmail.com>
2022-01-11 14:17:54 -0800

    Graph colors

--- frontend/components/graph.tsx
+++ frontend/components/graph.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import Neovis from "neovis.js";
+import Neovis, { NEOVIS_ADVANCED_CONFIG } from "neovis.js";
 
 const NEO4JURI = "neo4j://143.244.183.189";
 const NEO4JUSER = "neo4j";
@@ -10,12 +10,9 @@ const secondOrderQuery = (addr: string) => {
   RETURN acc, edge, child, edge2, second`;
 };
 
-const graph = ({}) => {
-  // TODO: don't use any
-  const visRef: any = useRef();
-
-  useEffect(() => {
-    const config = {
+// Old (v1) config
+/*
+const config = {
       container_id: visRef.current.id,
       server_url: NEO4JURI,
       server_user: NEO4JUSER,
@@ -25,7 +22,7 @@ const graph = ({}) => {
       ),
       labels: {
         Contract: {
-          caption: "addr",
+          caption: (node: any) => node.properties.addr.slice(0, 6),
         },
       },
       relationships: {
@@ -35,8 +32,60 @@ const graph = ({}) => {
         },
       },
     };
+*/
+
+const graph = ({}) => {
+  // TODO: don't use any
+  const visRef: any = useRef();
+
+  const user = "0x0fe35cb5c7d2dfd63eb4eb9e02a6f20117a3303b";
+
+  useEffect(() => {
+    const config = {
+      containerId: visRef.current.id,
+      neo4j: {
+        serverUrl: NEO4JURI,
+        serverUser: NEO4JUSER,
+        serverPassword: NEO4JPASS,
+      },
+      initialCypher: secondOrderQuery(
+        "0x0fe35cb5c7d2dfd63eb4eb9e02a6f20117a3303b"
+      ),
+      labels: {
+        User: {
+          [NEOVIS_ADVANCED_CONFIG]: {
+            function: {
+              color: (node: any) =>
+                node.properties.addr === user ? "yellow" : "blue",
+              title: (node: any) => node.properties.addr,
+            },
+          },
+        },
+        Contract: {
+          [NEOVIS_ADVANCED_CONFIG]: {
+            function: {
+              color: (node: any) => "red",
+              title: (node: any) => node.properties.addr,
+            },
+          },
+        },
+      },
+      relationships: {
+        To: {
+          value: "count",
+          [NEOVIS_ADVANCED_CONFIG]: {
+            function: {
+              color: (_: any) => "grey",
+            },
+          },
+        },
+      },
+    };
+    // Neovis provided types are wrong >:(
+    // @ts-ignore
     const vis = new Neovis(config);
     vis.render();
+    //vis.stabilize();
   }, []);
 
   return (
