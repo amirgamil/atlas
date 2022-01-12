6cce828fee6972ba70ba46c7cd961b6e4e62e391
amirgamil <amir.amgad01@gmail.com>
2022-01-11 19:40:05 -0800

    add server

---
+++ scraper/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import { generateRecommendationForAddr } from "./scraper/recommendation";
+
+const app = express();
+//CHANGE FOR PROD
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/recommend", async (req, res, next) => {
+    try {
+        const address: string = req.body.address;
+        const results = await generateRecommendationForAddr(address);
+        res.json({ results });
+    } catch (ex: unknown) {
+        console.log(ex);
+        res.status(503).send("Error occurred");
+    }
+});
+
+//make sure that the server is running
+app.listen(3001, () => {
+    console.log(`Server is up at port ${3001}`);
+});
