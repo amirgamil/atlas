f868bf0c3cf2662e8f8bab67769ca7ce4d2683a4
Jacky Zhao <j.zhao2k19@gmail.com>
2022-01-13 13:19:30 -0800

    secondary button

--- frontend/components/Splash.tsx
+++ frontend/components/Splash.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 
 const Splash = () => {
   return (
     <div className="text-white w-1/2 mx-auto my-40">
       <h1 className="text-6xl w-1/2">
-        <span className="text-gradient">Atlas</span> is the web3 recommendation engine.
+        <span className="text-gradient">Atlas</span> your map of web3.
       </h1>
       <h3 className="text-2xl my-10 font-normal">Get personalized recommendations on the newest NFTs, smart contracts, and DAOs. Sign in to get started.</h3>
-      <button>See what's hot →</button>
+      <Link href="/explore">
+        <a className="button">See what's hot →</a>
+      </Link>
     </div>
   )
 }
--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -15,14 +15,19 @@ const Nav = () => {
         />
         <h1 className="my-auto ml-4 text-3xl text-gradient glow">Atlas</h1>
       </div>
-      <button
-        className="ml-auto my-auto mr-16 cursor-pointer"
-        onClick={context.openModal}
-      >
-        {context.address
-          ? `Signed In: ${context.address.slice(0, 8)}...`
-          : "Sign In"}
-      </button>
+      <div className="ml-auto my-auto mr-16">
+        {context.address && <button className="secondary mx-4">
+          My Wallet
+        </button>}
+        <button
+          className="cursor-pointer"
+          onClick={context.openModal}
+        >
+          {context.address
+            ? `${context.address.slice(0, 8).toLowerCase()}...`
+            : "Sign In"}
+        </button>
+      </div>
     </div>
   );
 };
