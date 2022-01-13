0f04771a79f12c31b2a6b509e4f39ddcc073bd0a
Jacky Zhao <j.zhao2k19@gmail.com>
2022-01-13 09:42:54 -0800

    frontend checkpoint

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -1,19 +1,28 @@
 import { useAppContext } from "./context";
+import Image from 'next/image'
 
 const Nav = () => {
   const context = useAppContext();
 
   return (
-    <div className="w-full h-20 flex">
-      <div className="text-2xl my-auto ml-16">Metis</div>
-      <div
+    <div className="w-full pt-8 h-26 flex text-white">
+      <div className="text-2xl my-auto ml-16 flex">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={64}
+          height={64}
+        />
+        <h1 className="my-auto ml-4 text-3xl">Atlas</h1>
+      </div>
+      <button
         className="ml-auto my-auto mr-16 cursor-pointer"
         onClick={context.openModal}
       >
         {context.address
           ? `Signed In: ${context.address.slice(0, 8)}...`
           : "Sign In"}
-      </div>
+      </button>
     </div>
   );
 };
