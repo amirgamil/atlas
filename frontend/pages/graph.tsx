d0e20eb777a6ce973f8c6cbbe5f7311bb6f5b410
Matt Katz <mykatz01@gmail.com>
2022-01-14 10:29:37 -0800

    Remove old code

--- frontend/pages/graph.tsx
+++ frontend/pages/graph.tsx
@@ -9,7 +9,7 @@ const UserGraph = () => {
   console.log("address: ", context.address);
   return (
     <SafeHydrate>
-      <Graph user={"0xa335ade338308b8e071cf2c8f3ca5e50f6563c60"} />
+      <Graph user={context.address!} />
     </SafeHydrate>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -59,68 +59,6 @@ const Home: NextPage = () => {
       </Head>
       <main className={`overflow-y-auto ${styles.main}`}>
         {context.address ? <MyWallet address={context.address} hot={hot} /> : <Splash />}
-        {/*<div>*/}
-        {/*  <h1 className="text-center py-4">Transaction Graph</h1>*/}
-        {/*  <div className="mt-1">*/}
-        {/*    <div className="mb-2">*/}
-        {/*      <input*/}
-        {/*        type="text"*/}
-        {/*        name="name"*/}
-        {/*        id="name"*/}
-        {/*        className="h-16 ring-offset-2 outline-none ring-2 block w-full sm:text-sm p-4"*/}
-        {/*        placeholder="Your Address"*/}
-        {/*        value={address}*/}
-        {/*        onChange={(e) => setAddress(e.target.value)}*/}
-        {/*      />*/}
-        {/*    </div>*/}
-        {/*    <div className="w-full flex flex-col justify-center items-center mt-10">*/}
-        {/*      <button*/}
-        {/*        style={{*/}
-        {/*          background: "#4287f5",*/}
-        {/*          color: "white",*/}
-        {/*        }}*/}
-        {/*        className="rounded-md px-4 py-3 text-md"*/}
-        {/*        onClick={loadRecommendations}*/}
-        {/*      >*/}
-        {/*        Get recommendation*/}
-        {/*      </button>*/}
-        {/*      {loading ? <p>Learning your deepest darkest secrets</p> : null}*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*  <div className="text-center mt-6">*/}
-        {/*    {!loading && recommended.length > 0 ? (*/}
-        {/*      <div>*/}
-        {/*        <h1 className="text-center">Your Recommendations</h1>*/}
-        {/*        {recommended.map((a) => (*/}
-        {/*          <div>*/}
-        {/*            <a href={`https://etherscan.io/address/${a.addr}`}>*/}
-        {/*              {a.name ? a.name : a.addr}*/}
-        {/*            </a>*/}
-        {/*          </div>*/}
-        {/*        ))}*/}
-        {/*      </div>*/}
-        {/*    ) : null}*/}
-        {/*  </div>*/}
-        {/*  {address ? (*/}
-        {/*    <SafeHydrate>*/}
-        {/*      <Graph user={address} />*/}
-        {/*    </SafeHydrate>*/}
-        {/*  ) : null}*/}
-        {/*  <div className="text-center mt-6">*/}
-        {/*    {hot.length > 0 ? (*/}
-        {/*      <div>*/}
-        {/*        <h1 className="text-center">Hot Contracts</h1>*/}
-        {/*        {hot.map((a, i) => (*/}
-        {/*          <div key={i}>*/}
-        {/*            <a href={`https://etherscan.io/address/${a.addr}`}>*/}
-        {/*              {a.name ? a.name : a.addr}*/}
-        {/*            </a>*/}
-        {/*          </div>*/}
-        {/*        ))}*/}
-        {/*      </div>*/}
-        {/*    ) : null}*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </main>
     </div>
   );
