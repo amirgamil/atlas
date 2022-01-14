256b07ee530093ef3bc458f7057897aab96c46fe
amirgamil <amir.amgad01@gmail.com>
2022-01-13 18:04:49 -0800

    add graph

---
+++ frontend/pages/graph.tsx
@@ -0,0 +1,17 @@
+import dynamic from "next/dynamic";
+import React from "react";
+import { useAppContext } from "../components/Context";
+import SafeHydrate from "../components/SafeHydrate";
+const Graph = dynamic(() => import("../components/GraphView"), { ssr: false });
+
+const UserGraph = () => {
+  const context = useAppContext();
+  console.log("address: ", context.address);
+  return (
+    <SafeHydrate>
+      <Graph user={context.address} />
+    </SafeHydrate>
+  );
+};
+
+export default UserGraph;
