e7001418e024c082a912410f9bc7840aa68683d3
amirgamil <amir.amgad01@gmail.com>
2022-01-14 22:14:06 -0800

    add pretty stars

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,50 +1,48 @@
 import { useAppContext } from "./Context";
-import Image from 'next/image'
-import Link from 'next/link'
-import {useRouter} from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Nav = () => {
   const context = useAppContext();
-  const router = useRouter()
+  const router = useRouter();
 
   const goHome = () => {
-    router.push('/')
-  }
+    router.push("/");
+  };
 
   return (
-    <div className="w-full pt-8 h-26 flex text-white">
+    <div
+      style={{ zIndex: 40 }}
+      className="relative w-full pt-8 h-26 flex text-white"
+    >
       <div className="text-2xl my-auto ml-16 flex">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={50}
-          height={50}
-        />
+        <Image src="/logo.png" alt="Logo" width={50} height={50} />
         <Link href="/">
-          <h1 className="my-auto ml-4 text-3xl text-gradient glow"><a>Atlas</a></h1>
+          <h1 className="my-auto ml-4 text-3xl text-gradient glow">
+            <a>Atlas</a>
+          </h1>
         </Link>
       </div>
       <div className="ml-auto my-auto mr-16">
-        {context.address && <>
-          <Link href={`/graph?center=${context.address}`}>
-            <a className="button secondary mr-4">
-              Local Graph
-            </a>
-          </Link>
-          <Link href={`/explore`}>
-            <a className="button secondary mr-4">
-              Explore
-            </a>
-          </Link>
-          <button
-            className="my-auto cursor-pointer secondary mr-4"
-            onClick={context.signOut}
-          >
-            Sign Out
-          </button>
-        </>}
+        {context.address && (
+          <>
+            <Link href={`/graph?center=${context.address}`}>
+              <a className="button secondary mr-4">Local Graph</a>
+            </Link>
+            <Link href={`/explore`}>
+              <a className="button secondary mr-4">Explore</a>
+            </Link>
+            <button
+              className="my-auto cursor-pointer secondary mr-4"
+              onClick={context.signOut}
+            >
+              Sign Out
+            </button>
+          </>
+        )}
         <button
-          className="cursor-pointer"
+          className="cursor-pointer z-40"
           onClick={context.address ? goHome : context.openModal}
         >
           {context.address
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,14 +5,65 @@ import Nav from "../components/Nav";
 import { useAppContext } from "../components/Context";
 import Splash from "../components/Splash";
 import MyWallet from "../components/MyWallet";
+import * as React from "react";
 
 const Home: NextPage = () => {
   const context = useAppContext();
 
   context.attemptLogin();
+  React.useEffect(() => {
+    const canvas: any = document.getElementById("starfield");
+    const twinklers = [];
+    if (canvas) {
+      const points = [];
+      const ctx = canvas.getContext("2d"); // Change to 1 on retina screens to see blurry canvas.
+      let style_height = +getComputedStyle(canvas)
+        .getPropertyValue("height")
+        .slice(0, -2);
+      //get CSS width
+      let style_width = +getComputedStyle(canvas)
+        .getPropertyValue("width")
+        .slice(0, -2);
+      //scale the canvas
+      canvas.setAttribute("height", style_height * window.devicePixelRatio);
+      canvas.setAttribute("width", style_width * window.devicePixelRatio);
+      for (var i = 0; i < 1000; i++) {
+        const x = Math.round(
+          Math.random() * canvas.offsetWidth * window.devicePixelRatio
+        );
+        const y = Math.round(
+          Math.random() * canvas.offsetHeight * window.devicePixelRatio
+        );
+        ctx.beginPath();
+        ctx.arc(x + 0.5, y, 4 * Math.random(), 0, 2 * Math.PI, false);
+        const color = `rgb(255, 255, 255, ${Math.random()})`;
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+        // ctx.fillRect(x, y, 3 * Math.random(), 3 * Math.random());
+        if (i % 10 === 0) {
+          points.push({ x, y });
+        }
+      }
+    }
+  });
 
   return (
-    <div className={styles.container}>
+    <div style={{ position: "relative" }} className={styles.container}>
+      {!context.address ? (
+        <canvas
+          style={{
+            zIndex: 0,
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+          id="starfield"
+          className="absolute"
+        ></canvas>
+      ) : null}
       <Nav />
       <Head>
         <title>Atlas | Discover Web3</title>
@@ -23,7 +74,7 @@ const Home: NextPage = () => {
           rel="stylesheet"
         />
       </Head>
-      <main className={`overflow-y-auto ${styles.main}`}>
+      <main className={`overflow-y-auto ${styles.main} z-10`}>
         {context.address ? <MyWallet address={context.address} /> : <Splash />}
       </main>
     </div>
